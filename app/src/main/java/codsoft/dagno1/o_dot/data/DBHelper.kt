efd7f6c7c4818a7f54f072a8828db50e10ed7fa9
Dagim Tesfaye Mebrate <dagimcodes@gmail.com>
2023-09-19 18:15:28 +0300

    Added methods to db helper

--- app/src/main/java/codsoft/dagno1/o_dot/data/DBHelper.kt
+++ app/src/main/java/codsoft/dagno1/o_dot/data/DBHelper.kt
@@ -1,23 +1,24 @@
 package codsoft.dagno1.quotelytics.data
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import codsoft.dagno1.o_dot.data.Task
-import java.io.InputStream
 
 
 class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
-    private val inputStream: InputStream = context.assets.open("data.sql")
     override fun onCreate(db: SQLiteDatabase) {
         val query = ("CREATE TABLE " + TABLE_NAME + " ("
                 + ID + " INTEGER PRIMARY KEY, " +
                 TITLE + " TEXT," +
                 DESCRIPTION + " TEXT," +
-                DUE_DATE + " INTEGER" +
-                IS_CHECKED + " INT DEFAULT 0," +")")
+                DUE_DATE + " INTEGER," +
+                IS_FINISHED + " INT DEFAULT 0," +
+                FINISHED_DATE + " INTEGER" +")")
         db.execSQL(query)
     }
 
@@ -27,40 +28,67 @@ class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
     }
 
     fun addTask(task: Task) {
-
-        // below we are creating
-        // a content values variable
         val values = ContentValues()
-
-        // we are inserting our values
-        // in the form of key-value pair
         values.put(TITLE, task.title)
         values.put(DESCRIPTION, task.description)
-        values.put(IS_CHECKED, false)
+        values.put(IS_FINISHED, false)
         values.put(DUE_DATE, task.dudDate)
-
-        // here we are creating a
-        // writable variable of
-        // our database as we want to
-        // insert value in our database
         val db = this.writableDatabase
-
-        // all values are inserted into database
         db.insert(TABLE_NAME, null, values)
-
-        // at last we are
-        // closing our database
         db.close()
+    }
+    // Define a method in your DBHelper class
+    @SuppressLint("Range")
+    fun getTaskById(taskId: Int): Task? {
+        val db = readableDatabase
+        val cursor = db.query(
+            TABLE_NAME,
+            null,
+            "$ID = ?",
+            arrayOf(taskId.toString()),
+            null,
+            null,
+            null
+        )
+
+        return if (cursor.moveToFirst()) {
+            val id = cursor.getInt(cursor.getColumnIndex(ID))
+            val title = cursor.getString(cursor.getColumnIndex(TITLE))
+            val description = cursor.getString(cursor.getColumnIndex(DESCRIPTION))
+            val dueDate = cursor.getLong(cursor.getColumnIndex(DUE_DATE))
+            val isFinished = cursor.getInt(cursor.getColumnIndex(IS_FINISHED)) == 1
+            val finishedDate = cursor.getLong(cursor.getColumnIndex(FINISHED_DATE))
+
+            Task(id, title, description, dueDate, isFinished, finishedDate)
+        } else {
+            null // Task with the given ID not found
+        }
     }
 
 
-    fun markTaskAsFinished(taskId: Int): Boolean {
+    fun updateTask(updatedTask: Task): Boolean {
         val db = this.writableDatabase
 
         val values = ContentValues()
-        values.put(IS_CHECKED, 1)
+        values.put(TITLE, updatedTask.title)
+        values.put(DESCRIPTION, updatedTask.description)
+            values.put(DUE_DATE, updatedTask.dudDate)
+        values.put(IS_FINISHED, if (updatedTask.isFinished) 1 else 0)
+        values.put(FINISHED_DATE, updatedTask.finishedDate)
         val whereClause = "$ID = ?"
-        val whereArgs = arrayOf(taskId.toString())
+        val whereArgs = arrayOf(updatedTask.id.toString())
+        val rowsUpdated = db.update(TABLE_NAME, values, whereClause, whereArgs)
+        return rowsUpdated > 0
+    }
+    fun markTaskIsFinishedStatus(task: Task): Boolean {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(IS_FINISHED, if (task.isFinished) 1 else 0)
+        if (task.isFinished) {
+            values.put(FINISHED_DATE, task.finishedDate)
+        }
+        val whereClause = "$ID = ?"
+        val whereArgs = arrayOf(task.id.toString())
 
         val rowsUpdated = db.update(TABLE_NAME, values, whereClause, whereArgs)
         return rowsUpdated > 0
@@ -68,7 +96,67 @@ class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
 
 
 
-    companion object {
+    @SuppressLint("Range")
+    fun getAllUnfinishedTasks(): List<Task> {
+        val tasks = mutableListOf<Task>()
+        val db = this.readableDatabase
+
+        val cursor: Cursor = db.query(
+            TABLE_NAME,
+            null,
+            "$IS_FINISHED = 0",
+            null,
+            null,
+            null,
+            "$DUE_DATE ASC" // Order by due date ascending
+        )
+
+        while (cursor.moveToNext()) {
+            tasks.add(Task(
+                cursor.getInt(cursor.getColumnIndex(ID)),
+                cursor.getString(cursor.getColumnIndex(TITLE)),
+                cursor.getString(cursor.getColumnIndex(DESCRIPTION)),
+                cursor.getLong(cursor.getColumnIndex(DUE_DATE)),
+                cursor.getInt(cursor.getColumnIndex(IS_FINISHED)) == 1,
+                cursor.getLong(cursor.getColumnIndex(FINISHED_DATE))
+            ))
+        }
+        cursor.close()
+        return tasks
+    }
+
+    @SuppressLint("Range")
+    fun getAllFinishedTasks(): List<Task> {
+        val tasks = mutableListOf<Task>()
+        val db = this.readableDatabase
+
+        val cursor: Cursor = db.query(
+            TABLE_NAME,
+            null,
+            "$IS_FINISHED = 1",
+            null,
+            null,
+            null,
+            "$FINISHED_DATE DESC" // Order by finished date descending
+        )
+
+        while (cursor.moveToNext()) {
+            tasks.add(Task(
+                cursor.getInt(cursor.getColumnIndex(ID)),
+                cursor.getString(cursor.getColumnIndex(TITLE)),
+                cursor.getString(cursor.getColumnIndex(DESCRIPTION)),
+                cursor.getLong(cursor.getColumnIndex(DUE_DATE)),
+                cursor.getInt(cursor.getColumnIndex(IS_FINISHED)) == 1,
+                cursor.getLong(cursor.getColumnIndex(FINISHED_DATE))
+            ))
+        }
+
+        cursor.close()
+        return tasks
+    }
+
+
+companion object {
         private const val DATABASE_NAME = "O_DOT"
         private const val DATABASE_VERSION = 1
         const val TABLE_NAME = "tasks"
@@ -76,6 +164,7 @@ class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         const val TITLE = "title"
         const val DESCRIPTION = "description"
         const val DUE_DATE = "due_date"
-        const val IS_CHECKED = "is_checked"
+        const val IS_FINISHED = "is_finished"
+        const val FINISHED_DATE = "finished_date"
     }
 }
--- app/src/main/java/codsoft/dagno1/o_dot/data/Task.kt
+++ app/src/main/java/codsoft/dagno1/o_dot/data/Task.kt
@@ -1,11 +1,32 @@
 package codsoft.dagno1.o_dot.data
 
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 class Task(
     val id: Int,
-    val title: String,
-    val description: String,
-    val dudDate: String,
-    var isChecked: Boolean
-)
+    var title: String,
+    var description: String?,
+    var dudDate: Long,
+    var isFinished: Boolean,
+    var finishedDate: Long?
+) {
+    companion object {
+        private const val DATE_FORMAT = "MM/dd/yyyy"
+
+        fun timestampToDate(timestamp: Long): String {
+            val sdf = SimpleDateFormat(DATE_FORMAT, Locale.US)
+            val date = Date(timestamp)
+            return sdf.format(date)
+        }
+
+        fun dateToTimestamp(dateString: String): Long {
+            val sdf = SimpleDateFormat(DATE_FORMAT, Locale.US)
+            val date = sdf.parse(dateString)
+            return date?.time ?: 0L
+        }
+    }
+
+
+}
